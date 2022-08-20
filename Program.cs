449ef339254154a53e0d58738b2c76c0c4370387
Administrator <Administrator@PC-202107151145>
2022-08-20 20:41:16 +0800

    xin

--- Program.cs
+++ Program.cs
@@ -26,31 +26,6 @@ namespace picacomic
             Console.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss")}]{o.ToString()}");
         }
 
-        private static async Task PunchAsync(string username,string password,int index)
-        {
-            Log("=============================================");
-            Log($"开始运行第{index + 1}个账号");
-
-            Login login = await PicacomicUrl.Login(username, password);
-            Log("登录成功");
-            Header.SetAuthorization(login.Authorization);
-            Log("开始获取人物信息");
-            Profile profile = await PicacomicUrl.Profile();
-            Log($"昵称：{profile.User.Name}");
-            Log("开始签到");
-            Punch punch = await PicacomicUrl.Punch();
-            if (punch.PunchSuccess)
-            {
-                Log("签到完成");
-                Profile profile_punch = await PicacomicUrl.Profile();
-                Log($"等级：{profile_punch.User.Level}");
-                Log($"当前经验：{profile_punch.User.Exp}");
-            }
-            else
-            {
-                throw new Exception("签到失败");
-            }
-        }
 
         static string posts()
         {
